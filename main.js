f215f5068024cec6927e26fbbcf4c6397e1074b9
mahamjabbar456 <mahamjabbar320@gmail.com>
2024-03-22 12:40:39 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+"use strict";
+// Question 10 I used comment in question 7 and 8
+// Question 11
+let names = ["Maham", "Daniyal", "Aneek", "Mahnoor"];
+console.log(names[0]);
+console.log(names[1]);
+console.log(names[2]);
+console.log(names[3]);
+// Question 12
+for (let i = 0; i < names.length; i++) {
+    console.log(`My name is ${names[i]}`);
+}
